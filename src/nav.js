024056a380fb8c70e082e5f8ddf97625ca4497f3
techmeg <42649602+techmeg@users.noreply.github.com>
2022-03-25 18:26:41 -0400

    first-commit

---
+++ src/nav.js
@@ -0,0 +1,17 @@
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const title = document.querySelector(".title");
+
+navToggle.addEventListener("click", () => {
+  console.log("clicked");
+  const expanded = navToggle.getAttribute("aria-expanded") === "true" || false;
+  navToggle.setAttribute("aria-expanded", !expanded);
+  const navVisible = nav.getAttribute("data-visible") === "true" || false;
+  nav.setAttribute("data-visible", !navVisible);
+  const titleVisible = title.getAttribute("data-visible") === "true" || false;
+  title.setAttribute("data-visible", !titleVisible);
+  const transparent =
+    header.getAttribute("data-transparent") === "true" || false;
+  header.setAttribute("data-transparent", !transparent);
+});
